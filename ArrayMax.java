c7c97a87ecc4e80ad647f53bd46116407c3f0486
Ramya B <01fe22bca202@kletech.ac.in>
2024-02-10 10:12:19 +0530

    Code to find Biggest number for a given array

--- ArrayMax.java
+++ ArrayMax.java
@@ -1,9 +1,21 @@
+/**
+ * The ArrayMax class finds the largest element in an array and prints it.
+ */
 public class ArrayMax {
+    /**
+     * The main function finds the largest element in an array and prints it.
+     */
     public static void main(String[] args) {
         int[] arr = {10, 324, 45, 90, 9808};
         System.out.println("Largest in given array is " + findLargest(arr));
     }
   
+    /**
+     * The function finds the largest element in an array of integers.
+     * 
+     * @param arr The parameter "arr" is an array of integers.
+     * @return The method findLargest is returning the largest element in the given array.
+     */
     static int findLargest(int[] arr) {
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
